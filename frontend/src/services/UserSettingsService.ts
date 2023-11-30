e970516b2e0cdf9e7427c9a9b6667c90d619be71
Alex Diaconou <adiaconou@gmail.com>
2023-11-29 19:54:18 -0800

    Refactored UserSettingsService code

--- frontend/src/services/UserSettingsService.ts
+++ frontend/src/services/UserSettingsService.ts
@@ -7,49 +7,47 @@ const apiUrl = process.env.REACT_APP_BACKEND_URL;
 */
 export class UserSettingsService {
 
-    // Get UserSettings
+    // Get user's settings
     async getUserSettings(userId: string, token: string): Promise<UserSettings | null> {
-        console.log("URL: " + apiUrl + "/userSettings/" + userId);
         try {
-            const headers = new Headers();
-            headers.append('Authorization', `Bearer ${token}`);
-
-            const response = await fetch(`${apiUrl}/userSettings/${userId}`, { headers: headers });
-
-            // Check if the response status code indicates success
-            if (!response.ok) {
-                throw new Error(`Server responded with status ${response.status}`);
-            }
-
-            const userSettings: UserSettings = await response.json();
-            return userSettings;
+            const headers = this.createAuthHeaders(token);
+            const response = await this.sendRequest(`${apiUrl}/userSettings/${userId}`, { headers });
+            return await response.json();
         } catch (error) {
-            console.error("Error fetching user settings", error);
-            return null;
+            console.error("Error fetching UserSettings", error);
+            throw error;
         }
     }
 
-    // Update UserSettings
+    // Update user's settings
     async updateUserSettings(userSettings: UserSettings, token: string): Promise<void> {
         try {
-
-            const response = await fetch(
-                `${apiUrl}/userSettings/${userSettings.userId}`,
-                {
-                    method: "PUT",
-                    headers: {
-                        "Content-Type": "application/json",
-                        "Authorization": `Bearer ${token}`,
-                    },
-                    body: JSON.stringify(userSettings),
-                }
-            );
-
-            if (!response.ok) {
-                throw new Error("Error updating user settings");
-            }
+            const headers = this.createAuthHeaders(token);
+            headers.append("Content-Type", "application/json");
+            await this.sendRequest(`${apiUrl}/userSettings/${userSettings.userId}`, {
+                method: "PUT",
+                headers,
+                body: JSON.stringify(userSettings),
+            });
         } catch (error) {
-            console.error(`Error attempting to update user settings: ${error}`);
+            console.error("Error updating UserSettings", error);
+            throw error;
         }
+    }
+
+    // Send the http request to the server
+    private async sendRequest(url: string, options: RequestInit): Promise<Response> {
+        const response = await fetch(url, options);
+        if (!response.ok) {
+            throw new Error(`HTTP error status: ${response.status}`);
+        }
+        return response;
+    }
+
+    // Create the HTTP headers with auth
+    private createAuthHeaders(token: string): Headers {
+        const headers = new Headers();
+        headers.append('Authorization', `Bearer ${token}`);
+        return headers;
     }
 }
