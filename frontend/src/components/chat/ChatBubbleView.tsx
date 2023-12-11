6d9d732674516792459b1b5604980f84e389634b
Alex Diaconou <adiaconou@gmail.com>
2023-12-10 18:49:00 -0800

    Added translate icon to bot responses so user can eventually click and get a translation

--- frontend/src/components/chat/ChatBubbleView.tsx
+++ frontend/src/components/chat/ChatBubbleView.tsx
@@ -1,10 +1,13 @@
-import React from "react";
-import { Typography } from "@mui/material";
+import React, { useState } from "react";
+import { Typography, Popover } from "@mui/material";
 import Box from "@mui/material/Box";
 import Paper from "@mui/material/Paper";
 import { SxProps, Theme } from "@mui/system";
 import SmartToy from "@mui/icons-material/SmartToy";
 import TypingIndicator from "./TypingIndicatorView";
+import TranslateIcon from "@mui/icons-material/Translate";
+import { keyframes } from '@emotion/react';
+
 
 interface ChatBubbleViewProps {
   message: { text: string; isUser: boolean; };
@@ -14,6 +17,32 @@ interface ChatBubbleViewProps {
 
 const ChatBubbleView: React.FC<ChatBubbleViewProps> = ({ message, sx, waitingForMessageFromAI }) => {
   const textColor = "#ffffff";
+
+  const pulse = keyframes`
+  0% {
+    transform: scale(1);
+  }
+  50% {
+    transform: scale(1.15);
+  }
+  100% {
+    transform: scale(1);
+  }
+`;
+
+  const [anchorEl, setAnchorEl] = useState<HTMLElement | null>(null);
+
+  const handleTranslateClick = (event: React.MouseEvent<SVGSVGElement>) => {
+    //TODO: Implement call to openai to translate the text in quesiton and display in the popover
+    setAnchorEl(event.currentTarget as unknown as HTMLElement);
+  };
+
+  const handlePopoverClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
 
   return (
     <Box
@@ -56,16 +85,45 @@ const ChatBubbleView: React.FC<ChatBubbleViewProps> = ({ message, sx, waitingFor
             <TypingIndicator /> // Render TypingIndicator when waitingForMessageFromAI is true and message is not from the user
           ) : (
             <Typography
-            variant="body1"
-            style={{
-              fontFamily: "Noto Sans, monospace",
-              fontSize: "13px",
-              color: textColor,
-              whiteSpace: 'pre-line' // This will make CSS handle newlines as they are
-            }}
-          >
-            {message.text}
-          </Typography> )}
+              variant="body1"
+              style={{
+                fontFamily: "Noto Sans, monospace",
+                fontSize: "13px",
+                color: textColor,
+                whiteSpace: 'pre-line' // This will make CSS handle newlines as they are
+              }}
+            >
+              {message.text}
+            </Typography>)}
+          {!message.isUser && !waitingForMessageFromAI && ( // Only render for bot messages
+            <>
+              <div style={{ height: '10px' }} />  {/* New line for spacing */}
+              <TranslateIcon
+                aria-describedby={id}
+                onClick={handleTranslateClick}
+                sx={{
+                  cursor: 'pointer',
+                  color: 'white',
+                  fontSize: '1rem',
+                  alignSelf: 'flex-start',
+                  marginTop: 0.5,
+                  animation: `${pulse} 2s infinite`, // Apply the pulse animation here
+                }}
+              />
+              <Popover
+                id={id}
+                open={open}
+                anchorEl={anchorEl}
+                onClose={handlePopoverClose}
+                anchorOrigin={{
+                  vertical: 'bottom',
+                  horizontal: 'left',
+                }}
+              >
+                <Typography sx={{ p: 2 }}>Translated!</Typography>
+              </Popover>
+            </>
+          )}
         </Paper>
       </Box>
     </Box>
