d4eca4ffcd3a657ca84e1fc7fb54841e9ccdb68e
Alex Diaconou <adiaconou@gmail.com>
2024-01-14 08:05:08 -0800

    Removing unused imports

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,6 @@
 import { Route, Routes } from "react-router-dom";
-import React, { useState } from "react";
-import Box from "@mui/material/Box";
+import React from "react";
 import { AuthenticationGuard } from "./auth/authentication-guard";
-import { useAuth0 } from "@auth0/auth0-react";
 import LoginView from "./views/LoginView";
 import ChatView from "./views/ChatView";
 import SetupView from "./views/SetupView";
